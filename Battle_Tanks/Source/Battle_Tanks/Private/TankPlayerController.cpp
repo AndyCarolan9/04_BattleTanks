5547c980e3f634e11e03c9e4645eef66a2794c59
DESKTOP-PF0UA01\Andy Carolan <andycarolan9@gmail.com>
2018-06-23 17:31:48 +0100

    Screen pixel size found

--- Battle_Tanks/Source/Battle_Tanks/Private/TankPlayerController.cpp
+++ Battle_Tanks/Source/Battle_Tanks/Private/TankPlayerController.cpp
@@ -18,8 +18,38 @@ void ATankPlayerController::BeginPlay()
 	UE_LOG(LogTemp, Warning, TEXT("PlayerController Begin Play"));
 }
 
+void ATankPlayerController::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+	AimTowardsCrosshair();
+}
 
 ATank * ATankPlayerController::GetControlledTank() const
 {
 	return Cast<ATank>(GetPawn());
+}
+
+void ATankPlayerController::AimTowardsCrosshair()
+{
+	if (!GetControlledTank()) { return; }
+
+	FVector HitLocation; // out parameter
+	if (GetSightRayHitLocation(HitLocation))
+	{
+		UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+			// TODO tell controlled tank to aim at this point
+	}
+}
+
+// Get world location of linetrace through crosshair, true if it hits the landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	// find the crosshair position
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+	auto ScreenLocation = FVector2D(ViewportSizeX * CrossHairXLocation, ViewportSizeY * CrossHairYLocation);
+	UE_LOG(LogTemp, Warning, TEXT("ScreenLocation: %s"), *ScreenLocation.ToString());
+	// De-project the screen position of the crosshair to a world direction
+	// line-trace along that look direction, and see what we hit (up to max range)
+	return true;
 }
--- Battle_Tanks/Source/Battle_Tanks/Public/TankPlayerController.h
+++ Battle_Tanks/Source/Battle_Tanks/Public/TankPlayerController.h
@@ -15,8 +15,23 @@ class BATTLE_TANKS_API ATankPlayerController : public APlayerController
 {
 	GENERATED_BODY()
 
-public:
+private:
 	ATank * GetControlledTank() const;
 	
 	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+
+	// Start the tank moving the barrel so that a shot would hit where
+	// the crosshair intersects the world
+	void AimTowardsCrosshair();
+
+	// Return an out parameter, true if hit landscape
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairXLocation = 0.5;
+
+	UPROPERTY(EditAnywhere)
+	float CrossHairYLocation = 0.3333;
 };
